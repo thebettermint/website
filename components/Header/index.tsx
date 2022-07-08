47e71c87f79529503fd193ec70cf2c7dceb1fd77
interc0der <intercoder@whirled.io>
2022-07-07 22:43:17 -0700

    Update: revise header for website theme

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,109 +1,70 @@
-import React, {useState, ReactNode, MouseEventHandler, useEffect, useRef } from 'react'
-import { useStoreContext } from '../../store/store';
-import NavPanel from '../NavPanel'
-import NetworkPanel from '../NetworkPanel'
+import React, { useState, ReactNode, MouseEventHandler, useEffect, useRef } from 'react';
 
-import { MenuBurger, Cross, Bell, Globe, Search} from '../Icons';
+import { MenuBurger, Cross, Bell, Globe, Search } from '../Icons';
 
-import style from './index.module.scss'
+import ThemeToggle from '../Theme/themeToggle';
+import Title from '../Assets/images/svg/thebettermint/boilerplate.svg';
+import Logo from '../Assets/images/svg/thebettermint/logo.svg';
+
+import style from './index.module.scss';
+
+import { useStoreContext } from '../../context/store';
+import Panel from '../Panel';
 
 interface Props {
-    theme?:string,
-    invert?:boolean
-  };
-
-
-const Header = ({ theme, invert } :Props ) =>  {
-
-    const storeContext = useStoreContext()
-
-    const [ isDropdown, toggleDropdown ] = useState<Boolean>(false)
-    const [ isNetworkDropdown, toggleNetworkDropdown ] = useState<Boolean>(false)
-
-    const hamRef = useRef<HTMLDivElement>(null)
-
-    const dropdown = () => {
-        if (!isDropdown) return null
-        if (hamRef.current) hamRef.current.style.zIndex="9"
-        return <NavPanel theme={storeContext.theme} close={() => toggleDropdown(!isDropdown)}/>
-    }
-
-    const networkDropdown = () => {
-        if (!isNetworkDropdown) return null
-        if (hamRef.current) hamRef.current.style.zIndex="6"
-        return <NetworkPanel theme={storeContext.theme} close={() => toggleNetworkDropdown(!isNetworkDropdown)}/>
-    }
-
-
-    const handleMenuClick = () => toggleDropdown(!isDropdown)
-    const handleNetworkClick = () => {
-        console.log('clicked')
-        toggleNetworkDropdown(!isNetworkDropdown)}
-
-    let color = theme == 'light' ? 'rgb(61, 61, 61)' : '#ffffffff'
-    if (invert) color = '#ffffffff'
-
-    let crossColor = theme == 'light' ? 'black' : 'white'
-    
-    return (
-        <>
-        <div className={`${style.header} ${theme ? style[theme] : null}
-        ${invert ? style.invert : null}`}>
-        </div>
-
-        <div className={style.headerLeft}>
-                <div className={style.icon}>
-                    <Globe 
-                        color={color} 
-                        size={16} 
-                        onClick={handleMenuClick}/>
-                        </div>
-                        <div className={style.icon}>
-                    <Bell 
-                        color={color} 
-                        size={16} 
-                        onClick={handleMenuClick}/>
-                        </div>
-                        <div className={style.icon}>
-                    <Search 
-                        color={color} 
-                        size={16} 
-                        onClick={handleMenuClick}/>
-                        </div>
-            </div>
-
-        <div className={`${style.headerCenter} ${theme ? style[theme] : null}`}>
-                <div className={`${style.networkWrapper} 
-                ${isNetworkDropdown ? style.clear : null}  
-                ${storeContext.connection ? style.green : style.red}`}
-                onClick={handleNetworkClick}
-                >
-                    <div className={`${style.network} 
-                    ${invert 
-                        && theme == 'light' ? style.invert : null}
-                    ${isNetworkDropdown 
-                    && invert 
-                    && theme == 'light' ? style.forceFontColor : null}`}>{storeContext.activeNetwork.name}</div>
-                </div>
-            </div>
-
-        <div ref={hamRef} className={style.hamburger}>
-            { isDropdown ?  
-            <Cross 
-            color={crossColor} 
-            size={16} 
-            onClick={handleMenuClick}/>
-            :
-            <MenuBurger 
-            color={color} 
-            size={16} 
-            onClick={handleMenuClick}/>
-        }
-        </div>
-        {dropdown()}
-        {networkDropdown()}
-        </>
-    )
+  theme?: string;
+  invert?: boolean;
+  home?: any;
+  about?: any;
+  contact?: any;
+  join?: any;
 }
 
-export default Header
+const Header = ({ theme, invert, home, about, contact, join }: Props) => {
+  const hamRef = useRef<HTMLDivElement>(null);
+  const storeContext = useStoreContext();
+
+  const [isDropdown, toggleDropdown] = useState<Boolean>(false);
+
+  const dropdown = () => {
+    if (!isDropdown) return null;
+    if (hamRef.current) hamRef.current.style.zIndex = '9';
+    return <Panel close={() => toggleDropdown(!isDropdown)} />;
+  };
+
+  const handleMenuClick = () => toggleDropdown(!isDropdown);
+
+  return (
+    <>
+      <div className={`${style.header}`}></div>
+
+      <div className={style.headerLeft}>
+        <Title
+          className={style.title}
+          fill={storeContext.theme[0] === 'dark' ? 'white' : null}
+          width={'90%'}
+          height={42}
+        />
+        <Logo className={style.logo} width={'90%'} height={42} />
+      </div>
+
+      <div className={`${style.headerCenter} ${theme ? style[theme] : null}`}></div>
+
+      <div className={style.headerRight}>
+        <div className={style.items}>
+          <div onClick={() => home.current.scrollIntoView()}>home</div>
+          <div onClick={() => about.current.scrollIntoView()}>about</div>
+          <div onClick={() => join.current.scrollIntoView()}>join</div>
+          <div onClick={() => contact.current.scrollIntoView()}>contact</div>
+          <ThemeToggle />
+        </div>
+        <div className={style.ham} onClick={handleMenuClick}>
+          <MenuBurger fill={storeContext.theme[0] === 'dark' ? 'white' : null} size={24} />
+        </div>
+      </div>
+      {dropdown()}
+    </>
+  );
+};
+
+export default Header;
