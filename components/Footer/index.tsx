2491f4bdc053ec2c825dd35aa24a2ad5e57f2808
interc0der <intercoder@whirled.io>
2022-07-07 22:40:55 -0700

    Update: footer comps

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,23 +1,56 @@
-import React from 'react'
-import styles from './index.module.scss'
+import React from 'react';
+import styles from './index.module.scss';
 
-import Vercel from '../Assets/images/svg/vercel.svg'
+import Vercel from '../Assets/images/svg/vercel.svg';
 
-const Footer = () =>  {
-    return (
-        <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Vercel width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    )
-}
+import Logo from '../Assets/images/svg/thebettermint/logo.svg';
+import Title from '../Assets/images/svg/thebettermint/boilerplate.svg';
+import LinkLogo from '../Assets/images/png/thebettermint/horizontal.png';
 
-export default Footer
+import Image from 'next/image';
+
+import {
+  Discord,
+  Facebook,
+  Twitter,
+  Github,
+  Telegram,
+  Shield,
+  Bullseye,
+  Target,
+  Receipt,
+  TimeOclock,
+} from '../Icons';
+
+import { useStoreContext } from '../../context/store';
+
+const Footer = () => {
+  const storeContext = useStoreContext();
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.tag}>
+        <Title
+          fill={storeContext.theme[0] === 'dark' ? 'white' : null}
+          width={'90%'}
+          height={42}
+          style={{ margin: '10px' }}
+        />
+
+        <div className={styles.imgWrapper}>
+          <Image src={LinkLogo} height={154} width={668} />
+        </div>
+      </div>
+
+      <div className={styles.socials}>
+        <Discord fill={storeContext.theme[0] === 'dark' ? 'white' : null} size={24} />
+        <Facebook fill={storeContext.theme[0] === 'dark' ? 'white' : null} size={24} />
+        <Twitter fill={storeContext.theme[0] === 'dark' ? 'white' : null} size={24} />
+        <Telegram fill={storeContext.theme[0] === 'dark' ? 'white' : null} size={24} />
+        <Github fill={storeContext.theme[0] === 'dark' ? 'white' : null} size={24} />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
