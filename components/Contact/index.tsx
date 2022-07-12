5d2b97d9bc78d6c8a4c8ea7d62aad174ef7fda0b
interc0der <intercoder@whirled.io>
2022-07-12 14:19:10 -0700

    UCHI: handle submit button on form enter

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,16 +1,114 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './index.module.scss';
 import Input from '../Input';
 import Button from '../Button';
 
-const Contact = () => {
+import emailjs from 'emailjs-com';
+
+const Result = () => {
   return (
-    <div className={styles.form}>
+    <p className={styles.successMsg}>
+      Your Message has been successfully sent. We will contact you soon.
+    </p>
+  );
+};
+
+const ErrorMsg = () => {
+  return (
+    <p className={styles.errorMsg}>
+      Something went wrong. Make sure to include all information above.
+    </p>
+  );
+};
+
+const Contact = () => {
+  const [result, showresult] = useState<boolean>(false);
+  const [error, showerror] = useState<boolean>(false);
+
+  const orgRef = useRef<any>(null);
+  const firstRef = useRef<any>(null);
+  const lastRef = useRef<any>(null);
+  const emailRef = useRef<any>(null);
+  const aboutRef = useRef<HTMLTextAreaElement>(null);
+
+  const sendEmail = () => {
+    if (
+      !orgRef.current ||
+      !firstRef.current ||
+      !lastRef.current ||
+      !emailRef.current ||
+      !aboutRef.current
+    )
+      return showerror(true);
+
+    if (
+      orgRef.current.value === '' ||
+      firstRef.current.value === '' ||
+      lastRef.current.value === '' ||
+      emailRef.current.value === '' ||
+      aboutRef.current.value === ''
+    )
+      return showerror(true);
+
+    let data = {
+      org: orgRef.current.value,
+      first: firstRef.current.value,
+      last: lastRef.current.value,
+      email: emailRef.current.value,
+      about: aboutRef.current.value,
+    };
+
+    if (process.env.NEXT_PUBLIC_SERVICE_ID && process.env.NEXT_PUBLIC_TEMPLATE_ID)
+      emailjs
+        .send(
+          process.env.NEXT_PUBLIC_SERVICE_ID,
+          process.env.NEXT_PUBLIC_TEMPLATE_ID,
+          data,
+          process.env.NEXT_PUBLIC_USER_KEY
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            resetForm();
+            showresult(true);
+          },
+          (error) => {
+            console.log(error.text);
+            showerror(true);
+          }
+        );
+  };
+
+  const resetForm = () => {
+    if (
+      !orgRef.current ||
+      !firstRef.current ||
+      !lastRef.current ||
+      !emailRef.current ||
+      !aboutRef.current
+    )
+      return;
+
+    firstRef.current?.clear();
+    orgRef.current?.clear();
+    firstRef.current?.clear();
+    lastRef.current?.clear();
+    emailRef.current?.clear();
+    aboutRef.current.value = '';
+  };
+
+  setTimeout(() => {
+    showresult(false);
+    showerror(false);
+  }, 5000);
+
+  return (
+    <form className={styles.form}>
       <div className={styles.title}>get in touch</div>
 
       <div className={styles.row}>
         <Input
-          /*               ref={addressRef} */
+          ref={firstRef}
           className={styles.input}
           type="text"
           value=""
@@ -19,7 +117,7 @@ const Contact = () => {
           /*               onChange={handleChange} */
         />
         <Input
-          /*               ref={addressRef} */
+          ref={lastRef}
           className={styles.input}
           type="text"
           value=""
@@ -29,7 +127,7 @@ const Contact = () => {
         />
       </div>
       <Input
-        /*               ref={addressRef} */
+        ref={orgRef}
         className={styles.input}
         type="text"
         value=""
@@ -38,7 +136,7 @@ const Contact = () => {
         /*               onChange={handleChange} */
       />
       <Input
-        /*               ref={addressRef} */
+        ref={emailRef}
         className={styles.input}
         type="text"
         value=""
@@ -47,7 +145,7 @@ const Contact = () => {
         /*               onChange={handleChange} */
       />
       <textarea
-        /*               ref={addressRef} */
+        ref={aboutRef}
         className={styles.textarea}
         placeholder="tell us about you"
         /*               onChange={handleChange} */
@@ -58,11 +156,14 @@ const Contact = () => {
         theme={'light'}
         height={'36px'}
         width={'120px'}
-        onClick={() => console.log('clicked')}
+        onClick={() => sendEmail()}
         className={styles.button}>
         <div>submit</div>
       </Button>
-    </div>
+
+      <div className="rn-form-group">{result ? <Result /> : null}</div>
+      <div className="rn-form-group">{error ? <ErrorMsg /> : null}</div>
+    </form>
   );
 };
 
